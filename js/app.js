ffc3ca8534e433850dd7317dbaab1ccebdcab911
Barbara Tules <barbaratules@gmail.com>
2024-06-13 23:23:39 -0300

    ajuste na variavel repetida

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 function alterarStatus(numero){
-  let alugarBotao = document.getElementById(`game-${numero}`).querySelector('.dashboard__item__button')
-  let alugarImg = document.getElementById(`game-${numero}`).querySelector('.dashboard__item__img')
-  let informaAluguel = document.getElementById(`game-${numero}`).querySelector('.dashboard__item__button')
+  let gameClicado = document.getElementById(`game-${numero}`)
+  let alugarBotao = gameClicado.querySelector('.dashboard__item__button')
+  let alugarImg = gameClicado.querySelector('.dashboard__item__img')
 
   if(alugarBotao.classList.contains('dashboard__item__button--return') && alugarImg.classList.contains('dashboard__item__img--rented')){
     alugarBotao.classList.remove('dashboard__item__button--return')
     alugarImg.classList.remove('dashboard__item__img--rented')
-    informaAluguel.innerHTML = 'Alugar'
+    alugarBotao.innerHTML = 'Alugar'
   }else{
     alugarBotao.classList.add('dashboard__item__button--return')
     alugarImg.classList.add('dashboard__item__img--rented')
-    informaAluguel.innerHTML = 'Devolver'
+    alugarBotao.innerHTML = 'Devolver'
   }
 }
