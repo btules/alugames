96c38292b84658978dbd2d846144979cc13c8db1
Barbara Tules <barbaratules@gmail.com>
2024-06-17 21:17:15 -0300

    adicionar cofirmacao de devolucao e contagem de aluguel de jogos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados(){
+  console.log(`Total de jogos alugados: ${jogosAlugados}`)
+}
+
 function alterarStatus(numero){
   let gameClicado = document.getElementById(`game-${numero}`)
   let alugarBotao = gameClicado.querySelector('.dashboard__item__button')
   let alugarImg = gameClicado.querySelector('.dashboard__item__img')
+  let nomeJogo = gameClicado.querySelector('.dashboard__item__name')
 
   if(alugarBotao.classList.contains('dashboard__item__button--return') && alugarImg.classList.contains('dashboard__item__img--rented')){
-    alugarBotao.classList.remove('dashboard__item__button--return')
-    alugarImg.classList.remove('dashboard__item__img--rented')
-    alugarBotao.innerHTML = 'Alugar'
+    if(confirm(`Você tem certeza que quer devolver o jogo ${nomeJogo.textContent}?s`)){
+      alugarBotao.classList.remove('dashboard__item__button--return')
+      alugarImg.classList.remove('dashboard__item__img--rented')
+      alugarBotao.innerHTML = 'Alugar'
+      jogosAlugados--
+    }
   }else{
     alugarBotao.classList.add('dashboard__item__button--return')
     alugarImg.classList.add('dashboard__item__img--rented')
     alugarBotao.innerHTML = 'Devolver'
+    jogosAlugados++
   }
+
+  contarEExibirJogosAlugados()
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+  jogosAlugados = this.querySelectorAll('.dashboard__item__img--rented').length
+  contarEExibirJogosAlugados()
+})
